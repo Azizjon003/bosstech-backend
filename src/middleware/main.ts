457ff78cd235c8d57dab516f1e707b94dc218c18
Azizjon <aliqulovazizjon68@gmail.com>
2023-05-27 18:13:42 +0500

    rate limitter

--- src/middleware/main.ts
+++ src/middleware/main.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import cors from "cors";
 import messageRoute from "../routes/messages.routes.js";
+import rateLimit  from "express-rate-limit";
 import morgan from "morgan";
 const app = express();
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 app.use(morgan("dev"));
 app.use(
   cors({
